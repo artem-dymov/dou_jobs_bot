1070fcf83718a835016b0602f8a9f8f56a41919a
Artem Dymov <dymov.artem@outlook.com>
2023-08-10 16:06:50 +0300

    Trying to make selenium undetectable

--- src/parser/TabSession.py
+++ src/parser/TabSession.py
@@ -28,14 +28,14 @@ class TabSession:
         options.add_argument(user_agent)
         options.add_argument('--no-sandbox')
 
-        options.add_argument("start-maximized")
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        options.add_experimental_option('useAutomationExtension', False)
+        # options.add_argument("start-maximized")
+        # options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        # options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        # options.add_experimental_option('useAutomationExtension', False)
         options.add_argument('--disable-blink-features=AutomationControlled')
 
-        # options.headless = True
-        options.add_argument('--headless=new')
+        options.headless = True
+        # options.add_argument('--headless=')
 
         self.driver = webdriver.Remote(remote_chromedriver_link, options=options)
         self.driver.maximize_window()
